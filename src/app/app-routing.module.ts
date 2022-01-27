fb92c07fb7fc92328d49ad1edfc46937f9dcb667
Adler <adler@pop-os.localdomain>
2022-01-26 22:18:30 -0300

    formulario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { PropostaFormularioComponent } from './proposta-formulario/proposta-formulario.component';
 import { PropostaComponent } from './proposta/proposta.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'proposta', component: PropostaComponent }
+  { path: 'proposta', component: PropostaComponent },
+  { path: 'formularioProposta', component: PropostaFormularioComponent }
 ];
 
 @NgModule({
