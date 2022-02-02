fffbb67683fa1a54724e461da9e4202fa9096377
rafael duarte amaral <rafaelduarteamaral@pop-os.localdomain>
2022-02-01 22:31:15 -0300

    correção de pdf

--- src/app/pdf-proposta/pdf-proposta.component.ts
+++ src/app/pdf-proposta/pdf-proposta.component.ts
@@ -20,7 +20,7 @@ export class PdfPropostaComponent implements OnInit {
   }
 
   printPDF() {
-    let pdf = new jsPDF('p', 'pt', 'a4');
+    let pdf = new jsPDF('p', 'mm', 'A0');
     pdf.html(this.el.nativeElement, {
       callback: (pdf) => {
         pdf.save('propostas.pdf')
